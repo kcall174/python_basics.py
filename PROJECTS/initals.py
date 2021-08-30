871868d69b7e664bc93c95b7ba23322141630f89
kcall174 <kmacallaghan@gmail.com>
2021-08-30 11:15:18 -0400

    Create initals.py

---
+++ PROJECTS/initals.py
@@ -0,0 +1,23 @@
+#python3
+#initials.py - program that outputs users name into ascii art (not automated)
+
+
+#this is the first project codeacademy offers
+
+
+#name = Kenneth Call
+#funfact = I drink cold coffee only 
+
+name = '''
+K     K   CCC
+K    K   C   C
+K   K    C
+K K      C
+K   K    C
+K    K   C   C
+K     K   CCC 
+'''
+
+print(name)
+
+
