1255d8f5673e259268c6b58282fbeb6893c48995
kcall174 <kmacallaghan@gmail.com>
2021-08-30 13:03:20 -0400

    Create space.py

---
+++ PROJECTS/space.py
@@ -0,0 +1,46 @@
+#python3
+#codeacademy space.py project 
+
+"""
+Little Codey is an interplanetary space boxer, who is trying to win championship belts for various weight categories on other planets within the solar system. 
+Write a space.py program that helps him keep track of his target weight by:
+
+Checks which number planet is equal to.
+It should then compute his weight on the destination planet.
+
+Planet      Relative Gravity
+_____________________________
+Venus       0.91
+Mars        0.38
+Jupiter     2.34
+Saturn      1.06
+Uranus      0.92
+Neptune     1.19
+
+"""
+
+
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+weight = 185
+planet = 3
+
+# Write an if statement below:
+if planet == 1:
+  weight = weight * 0.91
+elif planet == 2:
+  weight = weight * 0.38
+elif planet == 3:
+  weight = weight * 2.34
+elif planet == 4:
+  weight = weight * 1.06
+elif planet == 5:
+  weight = weight * 0.92
+elif planet == 6:
+  weight = weight * 1.19
+ 
+print("Your weight:", weight)
+
