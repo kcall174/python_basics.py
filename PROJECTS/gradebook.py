9730bf44f0e0a4c99d8c5219ab64d037be1c551c
kcall174 <kmacallaghan@gmail.com>
2021-09-03 01:44:07 -0400

    Create gradebook.py

---
+++ PROJECTS/gradebook.py
@@ -0,0 +1,43 @@
+#python3
+# gradebook.py - enter in class subjects with relevant grading structure.
+
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+subjects = ["physics", "calculus", "poetry", "history"]
+
+grades = [98, 97, 85, 88]
+
+gradebook = [
+  ["physics", 98],
+  ["calculus", 97],
+  ["poetry", 85],
+  ["history", 88]
+]
+print(gradebook)
+
+gradebook.append(["computer science", 100])
+print(gradebook)
+
+gradebook.append(["visual arts", 93])
+print(gradebook)
+
+#add +5 to the number above
+gradebook[-1][1] = 98
+print(gradebook)
+
+gradebook[2].remove(85)
+print(gradebook)
+
+gradebook[2].append("Pass")
+print(gradebook)
+
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
+
+
+
+
+
+
+
