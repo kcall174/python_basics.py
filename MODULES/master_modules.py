1862719fe4cab778c22797343d6dd752091fab08
kcall174 <kmacallaghan@gmail.com>
2021-09-05 14:37:21 -0400

    Create master_modules.py

---
+++ MODULES/master_modules.py
@@ -0,0 +1,14 @@
+#python3
+#master_modules.py - this is a list of a lot of built-in python modules
+
+'''
+the entire list can be wound here: https://docs.python.org/3/py-modindex.html
+'''
+
+
+
+
+
+
+
+
