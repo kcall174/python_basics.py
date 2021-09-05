55e10d11b4802c740c2375bf75fe5254836ed8a9
kcall174 <kmacallaghan@gmail.com>
2021-09-05 15:17:40 -0400

    Update master_modules.py

--- MODULES/master_modules.py
+++ MODULES/master_modules.py
@@ -5,6 +5,15 @@
 the entire list can be wound here: https://docs.python.org/3/py-modindex.html
 '''
 
+#.pop()
+#removes elements of a list
+#.pop() will remove the last element automatically
+
+
+#.insert()
+#insert something somewhere
+#.instert(x, "info") will insert "info" at "x" index. 
+
 
 
 
