dd56720e19752fa9256632ad329c84cd720b9880
K.C <kmacallaghan@gmail.com>
2022-03-04 09:59:33 -0500

    Update Receipts.py

--- PROJECTS/Receipts.py
+++ PROJECTS/Receipts.py
@@ -1,5 +1,5 @@
-#python3
-#script.py - this program speeds up the process of reciepts
+#! python3
+#  script.py - this program speeds up the process of reciepts
 
 #Stores namesake
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
